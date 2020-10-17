d534f6896bd6bc40b2af0dd1856f09a4afe81da1
SpaNb4 <spanb4@gmail.com>
2020-10-17 20:52:19 +0300

    feat: add slider and farsing from json

--- shelter/pages/main/app.js
+++ shelter/pages/main/app.js
@@ -17,12 +17,16 @@ document.addEventListener("DOMContentLoaded", () => {
     // кнопка закрытия модального окна
     let modal_close = document.querySelector('[data-close-button]');
 
+    let pets_header = document.querySelector('.our_pets_header');
+
     menu_trigger.addEventListener('click', () => {
         logo.style.visibility = 'hidden';
         blackout.classList.toggle('active');
         menu_popup.classList.toggle('active');
         menu_close.classList.toggle('menu_close');
         menu_trigger.classList.toggle('menu_close');
+
+        pets_header.style.cssText = 'position: initial';
     }, false);
 
     menu_close.addEventListener('click', () => {
@@ -31,6 +35,8 @@ document.addEventListener("DOMContentLoaded", () => {
         menu_popup.classList.toggle('active');
         menu_close.classList.toggle('menu_close');
         menu_trigger.classList.toggle('menu_close');
+
+        pets_header.style.cssText = 'position: sticky';
     }, false);
 
     [].forEach.call(pets_items, function (pets_item) {
@@ -59,30 +65,69 @@ document.addEventListener("DOMContentLoaded", () => {
         modal_close.classList.remove('hover');
     }, false);
 
+    let pets_items_div = document.querySelector('.pets_items');
+    fetch('./pets.json')
+        .then(response => response.json())
+        .then(pets_json => {
+            let pets_obj = pets_json;
+            return pets_obj.map(function (pet) {
+                let pets_div = createNode('div');
+                pets_div.classList.add('pets_item');
 
-let slider = tns({
-    container: '.pets_items',
-    items: 3,
-    slideBy: 1,
-    fixedWidth: 355,
-    controlsContainer: '.control_buttons',
-    nav: false,
-    mouseDrag: true,
-    // responsive: {
-    //     320: {
-    //         items: 1,
-    //         fixedWidth: 270,
-    //     },
-    //     768: {
-    //         items: 2,
-    //         fixedWidth: 315, 
-    //     },
-    //     1280: {
-    //         items: 3,
-    //         fixedWidth: 355,
-    //     }, 
-    // }
-});
+                let img = createNode('img');
+                img.src = pet.img;
+
+                let pets_names_div = createNode('div');
+                pets_names_div.classList.add('pets_names');
+                pets_names_div.innerHTML = `${pet.name}`;
+
+                let pets_button = createNode('a');
+                pets_button.classList.add('btn-secondary');
+                pets_button.innerHTML = 'Learn more';
+                pets_button.href = '#';
+
+                append(pets_div, img);
+                append(pets_div, pets_names_div);
+                append(pets_div, pets_button);
+                append(pets_items_div, pets_div);
+
+            })
+        })
+        .then(() => {
+            let slider = tns({
+                container: '.pets_items.main_pets',
+                items: 3,
+                gutter: 90,
+                loop: false,
+                controlsContainer: '.control_buttons',
+                nav: false,
+                mouseDrag: true,
+                // responsive: {
+                //     320: {
+                //         items: 1,
+                //         fixedWidth: 270,
+                //     },
+                //     768: {
+                //         items: 2,
+                //         fixedWidth: 315, 
+                //     },
+                //     1280: {
+                //         items: 3,
+                //         fixedWidth: 355,
+                //     }, 
+                // }
+            })
+        });
+
+    function createNode(element) {
+        return document.createElement(element);
+    }
+
+    function append(parent, el) {
+        return parent.appendChild(el);
+    }
+
+
 
 
 });
